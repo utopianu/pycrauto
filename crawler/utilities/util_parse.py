6db0500cf0e5900f571031cbe6763e5d8dc6be51
utopianu <lylzzf3841@gmail.com>
2017-04-10 00:07:18 -0700

    .

--- crawler/utilities/util_parse.py
+++ crawler/utilities/util_parse.py
@@ -22,20 +22,6 @@ def get_string_num(string, base=None):
     if not temp:
         return 0.0
     num, param = float(temp.group("num")), temp.group("param")
-    if param.find("兆") >= 0:
-        num *= 10000000000000
-    if param.find("亿") >= 0:
-        num *= 100000000
-    if param.find("万") >= 0:
-        num *= 10000
-    if param.find("千") >= 0:
-        num *= 1000
-    if param.find("百") >= 0:
-        num *= 100
-    if param.find("十") >= 0:
-        num *= 10
-    if param.find("%") >= 0:
-        num /= 100
     if base:
         if param.find("K") >= 0:
             num *= base
